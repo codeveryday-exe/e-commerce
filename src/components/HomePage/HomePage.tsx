f83dab7f26eb82e6a4e66ebcc904a00c83c70c19
code <code65168@gmail.com>
2026-02-28 01:10:36 +0300

    feat: add trust section to homepage

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -2,6 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import styles from './HomePage.module.css';
 import { ProductCard } from '../ProductCard/ProductCard';
 import { fetchProducts } from '../../services/mock-shop';
+import { CornerDownLeft, Heart, Sparkle, Truck } from 'lucide-react';
 
 export function HomePage() {
   const {
@@ -31,6 +32,33 @@ export function HomePage() {
 
   return (
     <div className={styles.main_box}>
+      <section className={styles.trust_section}>
+        <div className={styles.trust_container}>
+          <div className={styles.trust_item}>
+            <Truck size={20} strokeWidth={1.5} />
+            <h4>Free Shipping</h4>
+            <p>On orders over $100</p>
+          </div>
+
+          <div className={styles.trust_item}>
+            <CornerDownLeft size={20} strokeWidth={1.5} />
+            <h4>Easy Returns</h4>
+            <p>30-day guarantee</p>
+          </div>
+
+          <div className={styles.trust_item}>
+            <Sparkle size={20} strokeWidth={1.5} />
+            <h4>Premium Fabrics</h4>
+            <p>Quality you can feel</p>
+          </div>
+
+          <div className={styles.trust_item}>
+            <Heart size={20} strokeWidth={1.5} />
+            <h4>Loved Worldwide</h4>
+            <p>Thousands of happy clients</p>
+          </div>
+        </div>
+      </section>
       <ul className={styles.products_container}>
         {products.map((product) => (
           <li key={product.id}>
